cd73bb7aa057ec118d78e60080804d623a1ed8a8
PatrickVir <114313628+PatrickVir@users.noreply.github.com>
2023-11-26 16:08:39 -0600

    Update data_base.ts
    Included Author and Book for Export

--- model/data_base.ts
+++ model/data_base.ts
@@ -1,6 +1,21 @@
+// database.ts
+
 export type User = {
-    id : number;
-    email: string;
-    name: string;
-    address: string;
-}
+  id: number;
+  email: string;
+  name: string;
+  address: string;
+};
+
+export type Author = {
+  id: number;
+  name: string;
+  books: Book[]; // Assuming Book type is also exported
+};
+
+export type Book = {
+  id: number;
+  title: string;
+  author: Author;
+  authorId: number;
+};
